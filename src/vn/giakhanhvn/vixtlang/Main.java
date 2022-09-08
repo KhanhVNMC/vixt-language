d348b5b9cebeb5954878d4fbd3c8a9994533c94d
GiaKhanhVN <71543494+KhanhVNMC@users.noreply.github.com>
2022-09-08 11:16:52 +0700

    Update Main.java

--- src/vn/giakhanhvn/vixtlang/Main.java
+++ src/vn/giakhanhvn/vixtlang/Main.java
@@ -1,21 +1,19 @@
 package vn.giakhanhvn.vixtlang;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
-import java.util.Random;
-
 import vn.giakhanhvn.vixtlang.compiler.Compiler;
 
 public class Main {
 	public static String VIXT_PATH = "C:\\Vixt\\";
 	
 	public static void main(String[] args) throws Exception {
+		if (!System.getProperty("os.name").toLowerCase().contains("windows")) {
+			VIXT_PATH = "/home";
+		}
 		StringBuilder s = new StringBuilder();
 		if (args.length <= 0) {
 			err("[!] Lenh Khong hop le! Yeu cau it nhat vi tri cua file can compile");
