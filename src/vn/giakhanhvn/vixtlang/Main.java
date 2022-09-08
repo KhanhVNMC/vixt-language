299995f1a704300e701fe9672c6cfab21fe7b3dd
GiaKhanhVN <71543494+KhanhVNMC@users.noreply.github.com>
2022-09-08 23:42:48 +0700

    Update Main.java

--- src/vn/giakhanhvn/vixtlang/Main.java
+++ src/vn/giakhanhvn/vixtlang/Main.java
@@ -1,22 +1,41 @@
 package vn.giakhanhvn.vixtlang;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.util.Scanner;
+
 import vn.giakhanhvn.vixtlang.compiler.Compiler;
 
 import sun.misc.Signal;
 import sun.misc.SignalHandler;
 
 public class Main {
+	public static final String VERSION = "v0.1.0-beta Build 20";
+	public static final int BUILD_VERSION = 1020;
+
 	public static String VIXT_PATH = "C:\\Vixt\\";
 	public static OSType OS = OSType.WINDOWS;
-	
+
 	public static void main(String[] args) throws Exception {
+		try {
+			Runtime.getRuntime().exec("javac -version");
+		} catch (Exception e) {
+			Main.out("[!] Khong tim thay JDK tren may cua ban! Vui long cai dat tai duong link nay\nhttps://www.oracle.com/java/technologies/downloads/#jdk18-windows\nBam enter de thoat!");
+			Scanner s = new Scanner(System.in);
+			s.nextLine();
+			s.close();
+			System.exit(0);
+			return;
+		}
 		long mil = System.currentTimeMillis();
-		if (!System.getProperty("os.name").toLowerCase().contains("windows")) {
+		if (!System.getProperty("os.name")
+			.toLowerCase().contains("windows")) {
 			VIXT_PATH = "";
 			Main.OS = OSType.UNIX;
 		}
@@ -28,13 +47,22 @@ public class Main {
 			Main.printHowTo();
 			return;
 		}
+		if (args[0].equalsIgnoreCase("-version")) {
+			Main.out("Vixt Development Kit (VDK) " + VERSION + "\nAuthor: GiaKhanhVN @ XiTrayTechnologies");
+			Main.out("Build Version Numeral ID: " + BUILD_VERSION);
+			return;
+		} else if (args[0].equalsIgnoreCase("-git")) {
+			Main.out("Author: GiaKhanhVN aka KhanhVNMC or Henser1255 @ XiTrayTechnologies");
+			Main.out("Official GitHub repository: https://github.com/KhanhVNMC/vixt-language");
+			return;
+		}
 		for (var a : args) {
 			s.append(a + " ");
 		}
 		boolean spaced = false;
 		if (s.charAt(0) == '"') spaced = true;
 		String fr = s.toString().split("\\.vx")[0] + ".vx" + (spaced ? "\"" : "");
-		File src = new File(fr);
+		File src = new File(new File(fr).getAbsolutePath());
 		if (!src.exists()) {
 			err("[!] Lenh Khong hop le! Khong tim thay file!");
 			err("File can compile phai la mot ma nguon Vixt hop le (duoi .vx)");
@@ -55,17 +83,35 @@ public class Main {
 	}
 	public static enum OSType {
 		WINDOWS,
-		UNIX
+		UNIX,
+		APPLE
 	}
+	
+	static String outStream(InputStream ins) throws Exception {
+		String line = null;
+		BufferedReader in = new BufferedReader(
+			new InputStreamReader(ins)
+		);
+		while ((line = in.readLine()) != null) {
+			return line;
+		}
+		return null;
+	}
+	
 	public static void printHowTo() {
+		out("Cach kiem tra cac thong tin");
+		out(" -version  Kiem tra phien ban hien tai cua Vixt Lang");
+		out(" -git  Mo GitHub chinh thuc cua Vixt PL");
+		out("Vi du: vixt -version");
+		out("");
 		out("Cach su dung: vixt tenfile.vx <cac flags, khong co cung duoc>");
 		out("");
 		out("Ngoai ra, con co cac Compiler flag nhu sau");
-		out("--clean   Don dep cac file tam thoi ngay lap tuc");
-		out("--silent   Han che cac console print cua phan mem");
-		out("--nv   Han che hoan toan console print cua phan mem");
-		out("--norun   Chi compile nhung khong interpret phan mem");
-		out("--debug   Bat che do debug phan mem");
+		out(" --clean   Don dep cac file tam thoi ngay lap tuc");
+		out(" --silent   Han che cac console print cua phan mem");
+		out(" --nv   Han che hoan toan console print cua phan mem");
+		out(" --norun   Chi compile nhung khong interpret phan mem");
+		out(" --debug   Bat che do debug phan mem");
 		out("Vi du:  vixt tenfile.vx --clean --silent");
 	}
 	public static void deleteDir(File file) {
@@ -91,7 +137,7 @@ public class Main {
 	public static void createOrLoadLibs() throws IOException {
 		File fld = new File(Main.VIXT_PATH + "libs/");
 		if (!fld.exists()) fld.mkdirs();
-		File sf = new File("libs/std.lyb");
+		File sf = new File(Main.VIXT_PATH, "libs/std.lyb");
 		if (!sf.exists()) {
 			sf.createNewFile();
 			FileWriter w = new FileWriter(Main.VIXT_PATH + "libs/std.lyb", StandardCharsets.UTF_8);
